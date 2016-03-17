99f0f25f8ed7484cef439c09957863480642f545
Anna Lukinykh <alukinykh@crystalnix.com>
2016-03-17 20:25:53 +0600

    comment myapp.run

--- client/main.js
+++ client/main.js
@@ -32,13 +32,13 @@ myApp.config(function ($routeProvider) {
 
 myApp.run(function ($rootScope, $location, $route, AuthService) {
   
-  $rootScope.$on('$routeChangeStart',
-    function (event, next, current) {
-      AuthService.getUserStatus();
-      if (next.access.restricted &&
-          !AuthService.isLoggedIn()) {
-        $location.path('/login');
-        $route.reload();
-      }
-  });
+  // $rootScope.$on('$routeChangeStart',
+  //   function (event, next, current) {
+  //     AuthService.getUserStatus();
+  //     if (next.access.restricted &&
+  //         !AuthService.isLoggedIn()) {
+  //       $location.path('/login');
+  //       $route.reload();
+  //     }
+  // });
 });
