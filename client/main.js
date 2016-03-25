d7c9187f0358a7e508d9fdfc008aac4b86f894d4
Anna Lukinykh <alukinykh@crystalinx.com>
2016-03-25 20:08:14 +0600

    fix login form

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ui.router']);
+var myApp = angular.module('myApp', ['ngMaterial', 'ui.router']);
 
 myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $stateProvider
