e814c488f1543a92e67457c089a65bfe74deb6ca
Anna Lukinykh <alukinykh@crystalinx.com>
2016-03-31 18:33:47 +0600

    add board add function createBoard

--- server/models/board.js
+++ server/models/board.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Board = new Schema({
-    name: String,
+    name: {	type: String, unique: true },
     description: String,
     user_id: String
 });
