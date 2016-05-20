5265cfbf85970ece62b6e8d131c7c4286b78243e
Anna Lukinykh <alukinykh@crystalnix.com>
2016-05-20 10:57:02 +0600

    Fix delete posts add pre remove function in model to delete comments of post

--- server/models/post.js
+++ server/models/post.js
@@ -13,4 +13,24 @@ var Post = new Schema({
     user_dislike: Array
 });
 
+Post.pre('remove', function(next) {
+    // 'this' is the client being removed. Provide callbacks here if you want
+    // to be notified of the calls' result.
+    //Vouchers.remove({user_id: this._id}).exec();
+    var id = this._id;
+    mongoose.model("Post").findOne({post_id: id}, function(err, post) {
+        var post_json = JSON.stringify(post);
+        Post.remove({_id: post_json._id});
+        console.log("pre test", id);
+        next();
+    });
+    //Post.find({post_id: this._id}), function(err, posts) {
+    //    console.log(posts);
+    //    //posts.remove();
+    //});
+    next();
+
+});
+
 module.exports = mongoose.model('Post', Post);
+
