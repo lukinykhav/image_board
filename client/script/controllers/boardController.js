aa13a56322ee0ceffe58d86a5ed4fa6bdb94256f
Anna Lukinykh <alukinykh@crystalnix.com>
2016-10-08 15:21:35 +0600

    Recomment function in boardController

--- client/script/controllers/boardController.js
+++ client/script/controllers/boardController.js
@@ -11,22 +11,21 @@ angular.module('myApp').controller('boardController',
             var token = localStorage.getItem('token');
 
             BoardService.getBoard(id)
-            // BoardService.getBoard(id)
-            //     .then(function successCallback(data) {
-            //         filtred = filter.filterPosts(data.posts);
-            //         $scope.board_name = data.board.name;
-            //         $scope.posts = filtred.posts;
-            //         $scope.comments = filtred.comments;
-            //         dataHolder.updateValue(data.board._id);
-            //         PostService.getUserPost(id, token)
-            //             .then(function (data) {
-            //                 $scope.userRole = data[1];
-            //                 $scope.changePost = data[0];
-            //             })
-            //     },
-            //     function errorCallback(err) {
-            //         console.log(err);
-            //     });
+                .then(function successCallback(data) {
+                    filtred = filter.filterPosts(data.posts);
+                    $scope.board_name = data.board.name;
+                    $scope.posts = filtred.posts;
+                    $scope.comments = filtred.comments;
+                    dataHolder.updateValue(data.board._id);
+                    PostService.getUserPost(id, token)
+                        .then(function (data) {
+                            $scope.userRole = data[1];
+                            $scope.changePost = data[0];
+                        })
+                },
+                function errorCallback(err) {
+                    console.log(err);
+                });
 
             $scope.editPost = function (post_id) {
                 $mdDialog.show({
