08b63a1047905b23a52205d17f6e5627896af626
Anna Lukinykh <alukinykh@crystalnix.com>
2016-12-24 16:50:34 +0600

    Fix page-proof
    Fix board, post page for mobile version.

--- client/script/controllers/editPostController.js
+++ client/script/controllers/editPostController.js
@@ -1,5 +1,6 @@
 angular.module('myApp').controller('editPostController',
     ['$scope', '$http', 'locals', '$mdDialog', 'PostService', function ($scope, $http, locals, $mdDialog, PostService) {
+        
 
         $scope.cancel = function() {
             $mdDialog.cancel();
