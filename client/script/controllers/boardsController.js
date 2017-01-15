aef40201ffea331c39fda2d2f2c972ec49004347
Anna Lukinykh <alukinykh@crystalnix.com>
2017-01-15 21:33:25 +0600

    Fix adding board

--- client/script/controllers/boardsController.js
+++ client/script/controllers/boardsController.js
@@ -4,19 +4,21 @@ angular.module('myApp').controller('boardsController',
 
             $scope.showAddBoardForm = function () {
                 $scope.addBoardForm = !$scope.addBoardForm;
-                $scope.listBoard();
+                //$scope.listBoard();
             };
 
             // $scope.listBoard = function () {
                 BoardService.listBoard()
                     .then(function (data) {
                         $scope.boards = data;
-                    })
+                    });
             // };
 
             $scope.addBoard = function () {
                 BoardService.addBoard($scope.name, $scope.description)
                     .then(function (data) {
+                        $scope.boards.push(data.data);
+                        $scope.addBoardForm = !$scope.addBoardForm;
                     })
                     .catch(function () {
                         $scope.errorMessage = "Error";
