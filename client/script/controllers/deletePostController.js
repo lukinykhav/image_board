cf5b62afbf9881c47f82e788179a26eccd74beaf
Anna Lukinykh <alukinykh@crystalnix.com>
2017-01-21 17:51:45 +0600

    Fix bugs
    Fix display comments button, fix remove comments and hide them.

--- client/script/controllers/deletePostController.js
+++ client/script/controllers/deletePostController.js
@@ -7,11 +7,37 @@ angular.module('myApp').controller('deletePostController',
                         $state.go('user.board', {id: data.post.board_id})
                     }
                     for (var i = 0; i < $scope.posts.length; i++) {
+                        var child_id = [];
+                        function findChildren (id) {
+                            for (var i = 0; i < $scope.posts.length; i++) {
+                                if ($scope.posts[i]._id === id) {
+                                    if($scope.posts[i]['children'] !== null) {
+                                        var arr = $scope.posts[i]['children'].split(',');
+                                        for (var j = 0; j < arr.length; j++) {
+                                            child_id.push(arr[j]);
+                                            findChildren(arr[j]);
+                                        }
+                                    }
+                                    else {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
                         if (data.hasOwnProperty('parent') && $scope.posts[i]['_id'] === data.parent._id) {
                             $scope.posts[i]['children'] = data.parent.children;
                         }
                         if ($scope.posts[i]['_id'] === id) {
+                            findChildren(id);
                             $scope.posts.splice(i, 1);
+                            var i = 0;
+                            while(i < $scope.posts.length) {
+                                if (child_id.indexOf($scope.posts[i]['_id']) > -1) {
+                                    $scope.posts.splice(i, 1);
+                                    continue;
+                                }
+                                i++;
+                            }
                         }
                     }
                 })
--- client/script/controllers/postController.js
+++ client/script/controllers/postController.js
@@ -63,7 +63,6 @@ angular.module('myApp').controller('postController',
                     $rootScope.arr_display.push(post_id);
                 }
                 else {
-                    $rootScope.arr_display.splice($rootScope.arr_display.indexOf(post_id), 1);
                     var child_id = [];
                     function findChildren (id) {
                         for (var i = 0; i < $scope.posts.length; i++) {
@@ -85,113 +84,16 @@ angular.module('myApp').controller('postController',
                     var i = 0;
                     while(i < $scope.posts.length) {
                         if (child_id.indexOf($scope.posts[i]['_id']) > -1) {
-                            $rootScope.arr_display.splice($rootScope.arr_display.indexOf($scope.posts[i]['_id']), 1);
+                            if ($rootScope.arr_display.indexOf($scope.posts[i]['_id']) > -1) {
+                                $rootScope.arr_display.splice($rootScope.arr_display.indexOf($scope.posts[i]['_id']), 1);
+                            }
                             $scope.posts.splice(i, 1);
                             continue;
                         }
                         i++;
                     }
+                    $rootScope.arr_display.splice($rootScope.arr_display.indexOf(post_id), 1);
                 }
-                //var child_id = [];
-                //function findChildren (id) {
-                //    for (var i = 0; i < $scope.posts.length; i++) {
-                //        if ($scope.posts[i]._id === id) {
-                //            if($scope.posts[i]['children'] !== null) {
-                //                var arr = $scope.posts[i]['children'].split(',');
-                //                console.log(arr);
-                //                for (var j = 0; j < arr.length; j++) {
-                //                    child_id.push(arr[j]);
-                //                    findChildren(arr[j]);
-                //                    //    //if ($scope.posts[i]._id === arr[j]) {
-                //                    //    //    console.log($scope.posts[i]);
-                //                    //    //}
-                //                }
-                //            }
-                //            else {
-                //                return true;
-                //            }
-                //        }
-                //        else {
-                //            PostService.getComments(post_id)
-                //                .then(function (data) {
-                //                    var index, arr_posts_id = [];
-                //
-                //                    for (var j = 0; j < $scope.posts.length; j++) {
-                //                        arr_posts_id.push($scope.posts[j]['_id']);
-                //                        if(post_id === $scope.posts[j]['_id']) {
-                //                            index = j;
-                //                        }
-                //                    }
-                //
-                //                    for (var i = 0; i < data.length; i++) {
-                //                        if(arr_posts_id.indexOf(data[i]._id) === -1) {
-                //                            data[i]['class'] = 'comment';
-                //                            $scope.posts.splice(index+1, 0, data[i]);
-                //                            $scope.displayComments = true;
-                //                        }
-                //                        //else {
-                //                        //    $scope.posts.splice(index+1, 1);
-                //                        //    $scope.displayComments = false;
-                //                        //}
-                //                    }
-                //                });
-                //        }
-                //    }
-                //}
-                //findChildren(post_id);
-                //for (var i = 0; i < $scope.posts.length; i++) {
-                //    console.log(child_id.indexOf($scope.posts[i]['_id']));
-                //    if (child_id.indexOf($scope.posts[i]['_id'] > -1)) {
-                //        $scope.posts.splice(i, 1)
-                //    }
-                //}
-                //findChildren('5879fbec906d609e11a45daa');
-                //console.log(child_id);
-
-                //for (var i = 0; i < $scope.posts.length; i++) {
-                //    if (child_id.indexOf($scope.posts[i]['_id'] !== -1)) {
-                //        console.log($scope.posts[i]);
-                //    }
-                //    //if ($scope.posts[i]._id === post_id && $scope.posts[i]['children'] !== null) {
-                //    //    var arr = $scope.posts[i]['children'].split(',');
-                //    //    console.log(arr);
-                //    //    for (var j = 0; j < arr.length; j++) {
-                //    //        console.log(arr[j]);
-                //    //    }
-                //    //}
-                //}
-
-
-                //if ($rootScope.arr_display.indexOf(post_id) === -1) {
-                //    $rootScope.arr_display.push(post_id);
-                //}
-                //else {
-                //    $rootScope.arr_display.splice($rootScope.arr_display.indexOf(post_id), 1);
-                //}
-
-                //PostService.getComments(post_id)
-                //    .then(function (data) {
-                //        var index, arr_posts_id = [];
-                //
-                //        for (var j = 0; j < $scope.posts.length; j++) {
-                //            arr_posts_id.push($scope.posts[j]['_id']);
-                //            if(post_id === $scope.posts[j]['_id']) {
-                //                index = j;
-                //            }
-                //        }
-                //
-                //        for (var i = 0; i < data.length; i++) {
-                //            if(arr_posts_id.indexOf(data[i]._id) === -1) {
-                //                data[i]['class'] = 'comment';
-                //                $scope.posts.splice(index+1, 0, data[i]);
-                //                $scope.displayComments = true;
-                //            }
-                //            else {
-                //                $scope.posts.splice(index+1, 1);
-                //                $scope.displayComments = false;
-                //            }
-                //        }
-                //    });
             };
         }
     ]
